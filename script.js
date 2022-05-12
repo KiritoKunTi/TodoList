b3fae7acf850da9d5ef3d88eef54b09d6e42db3d
Aisayev Duman <duman200354@mail.ru>
2022-05-12 13:37:44 +0600

    ready

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
+const inputNode = document.getElementById('add-task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const nodeTasks = document.getElementById('tasks');
 
-window.addEventListener('load', () => {
-    const inputNode = document.getElementById('add-task-input');
-    const addTaskBtn = document.getElementById('add-task-btn');
-    const nodeTasks = document.getElementById('tasks');
+addTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-    addTaskBtn.addEventListener('click', (e) => {
-        e.preventDefault();
+    let currentTask = inputNode.value;
 
-        let currentTask = inputNode.value;
+    if (!currentTask) {
+        alert('Please, add task!');
+        return;
+    }
 
-        if (!currentTask) {
-            alert('Please, add task!');
-            return;
-        }
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
 
-        const task_el = document.createElement("div");
-        task_el.classList.add('box-task');
+    if (localItems === null) {
+        taskList = [];
+    } else {
+        taskList = localItems;
+    }
 
-        const task_text = document.createElement('div');
-        task_text.classList.add('place-center');
-        task_text.classList.add('task');
-        task_text.innerText = currentTask;
+    taskList.push(inputNode.value);
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    setTasks();    
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add('delete-btn');
-        deleteBtn.innerText = 'Delete';
+    inputNode.value = '';
 
-        task_el.appendChild(task_text);
-        task_el.appendChild(deleteBtn);
-        nodeTasks.appendChild(task_el);
-
-        inputNode.value = '';
-
-        deleteBtn.addEventListener('click', () => {
-            nodeTasks.removeChild(task_el);
-        });
-
+    deleteBtn.addEventListener('click', () => {
+        nodeTasks.removeChild(task_el);
     });
 
 });
 
+function setTasks() {
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+    if (localItems === null) {
+        taskList = [];
+    } else {
+        taskList = localItems;
+    }
+
+    let nodeTask = '';
+    const itemShow = document.querySelector('.tasks');
+
+    taskList.forEach((task, index) => {
+        nodeTask += 
+            `<div class="box-task">
+                <div class="task place-center">${task}</div>
+                <button class="delete-btn" onclick="deleteTask(${index})">Delete</button>
+            </div>`;
+    });
+
+    itemShow.innerHTML = nodeTask;
+}   
+
+setTasks();
+
+function deleteTask(index) {
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+    taskList.splice(index, 1);
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    setTasks();
+};
